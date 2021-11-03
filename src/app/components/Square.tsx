ca90bb1fdafcc22e3aa929b497a3e7ca8f536984
Julia Loeschel <juloeschel@gmail.com>
2021-11-03 14:41:58 +0100

    finish

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 
-function Square(props: {
-  onClick: () => void;
-  value:
-    | boolean
-    | React.ReactChild
-    | React.ReactFragment
-    | React.ReactPortal
-    | null
-    | undefined;
-}) {
+function Square(props: { onClick: () => void; value: string }): JSX.Element {
   return (
     <button className="square" onClick={() => props.onClick()}>
       {props.value}
